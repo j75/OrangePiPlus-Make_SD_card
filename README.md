ef0366398fd7ec976ce7665cdd98665814eeb441
Marian-N. Ion <marian@famion.eu>
2016-04-13 07:54:24 +0200

    Using proprietary boot loader.

--- README.md
+++ README.md
@@ -18,45 +18,143 @@ run Linux on a OrangePi Plus board.
   * [Setting up the Linux distribution root file system](http://www.orangepi.org/Docs/SettinguptheLinux.html)
 
 ## Issues
-  + boots, but stops after loading kernel: <keyword>
+  + boots, but does not load kernel: <keyword>
   ```
-U-Boot SPL 2016.03-00394-gd085ecd-dirty (Mar 25 2016 - 19:07:28)
-DRAM: 1024 MiB
-Trying to boot from MMC
+HELLO! BOOT0 is starting!
+boot0 version : 4.0.0
+fel_flag = 0x00000000
+rtc[0] value = 0x00000000
+rtc[1] value = 0x00000000
+rtc[2] value = 0x00000000
+rtc[3] value = 0x00000000
+rtc[4] value = 0x00000000
+rtc[5] value = 0x00000000
+rtc[6] value = 0x00000000
+rtc[7] value = 0x00000000
+DRAM DRIVE INFO: V0.8
+DRAM Type = 3 (2:DDR2,3:DDR3,6:LPDDR2,7:LPDDR3)
+DRAM CLK = 672 MHz
+DRAM zq value: 003b3bfb
+READ DQS LCDL = 001c1c1c
+DRAM SIZE =1024 M
+odt delay 
+dram size =1024
+card boot number = 0
+card no is 0
+sdcard 0 line count 4
+[mmc]: mmc driver ver 2014-12-10 21:20:39
+[mmc]: ***Try SD card 0***
+[mmc]: SD/MMC Card: 4bit, capacity: 1876MB
+[mmc]: vendor: Man 0002544d Snr 9c100113
+[mmc]: product: SA02G
+[mmc]: revision: 0.4
+[mmc]: ***SD/MMC 0 init OK!!!***
+sdcard 0 init ok
+The size of uboot is 000dc000.
+sum=6f0b4bf6
+src_sum=6f0b4bf6
+Succeed in loading uboot from sdmmc flash.
+Ready to disable icache.
+Jump to secend Boot.
+SUNXI_NORMAL_MODE   
+already secure mode
 
-U-Boot 2016.03-00394-gd085ecd-dirty (Mar 25 2016 - 19:07:28 +0100) Allwinner Technology
+[      0.349]
 
-CPU:   Allwinner H3 (SUN8I)
-Model: Xunlong Orange Pi Plus
-I2C:   ready
-DRAM:  1 GiB
-MMC:   SUNXI SD/MMC: 0
-*** Warning - bad CRC, using default environment
+U-Boot 2011.09-rc1-00000-g0cc8d85-dirty (Apr 05 2016 - 21:00:55) Allwinner Technology 
+
+[      0.358]version: 1.1.0
+normal mode
+[      0.365]pmbus:   ready
+not set main pmu id
+axp_probe error
+[      0.380]PMU: pll1 1536 Mhz,PLL6=600 Mhz
+AXI=512 Mhz,AHB=200 Mhz, APB1=100 Mhz 
+sid read already 
+fel key new mode
+run key detect
+no key found
+no key input
+dram_para_set start
+dram_para_set end
+normal mode
+[      0.410]DRAM:  1 GiB
+relocation Offset is: 35af9000
+[box standby] read rtc = 0x0
+[box_start_os] mag be start_type no use
+user_gpio config
+user_gpio ok
+gic: normal or no secure os mode
+workmode = 0
+MMC:	 0
+[      0.482][mmc]: mmc driver ver 2014-12-10 9:23:00
+[      0.487][mmc]: get sdc_phy_wipe fail.
+[      0.491][mmc]: get sdc0 sdc_erase fail.
+[      0.495][mmc]: get sdc_f_max fail,use default 50000000Hz
+[      0.500][mmc]: get sdc_ex_dly_used fail,use default dly
+[      0.506][mmc]: SUNXI SD/MMC: 0
+[      0.519][mmc]: *Try SD card 0*
+[      0.552][mmc]: CID 0x2544d53 0x41303247 0x49c1001 0x1300a4db
+[      0.557][mmc]: mmc clk 50000000
+[      0.560][mmc]: SD/MMC Card: 4bit, capacity: 1876MB
+[      0.565][mmc]: boot0 capacity: 0KB,boot1 capacity: 0KB
+[      0.571][mmc]: ***SD/MMC 0 init OK!!!***
+[      0.576][mmc]: erase_grp_size:0x1WrBlk * 0x200 = 0x200 Byte
+[      0.582][mmc]: secure_feature 0x0
+[      0.585][mmc]: secure_removal_type  0x0
+[      0.589]sunxi flash init ok
+script config pll_de to 864 Mhz
+Not Found clk pll_video1 in script 
+script config pll_video to 297 Mhz
+[boot]disp_init_tv
+[DISP_TV] disp_init_tv enter g_tv_used
+screen 0 do not support TV TYPE!
+[BOOOT_DISP_TV] disp tv device_registered
+unable to find regulator vcc-hdmi-18 from [pmu1_regu] or [pmu2_regu] 
+enable power vcc-hdmi-18, ret=-1
+DRV_DISP_Init end
+boot_disp.auto_hpd=1
+auto hpd check has 100 times!
+auto check no any connected, the output_type is 4
+[disk_read_fs] no the partition
+error: open disp_rsl.fex, maybe it is not exist
+not support this mode[4], use inline mode[4]
+attched ok, mgr0<-->device0, type=4, mode=4----
+ready to set mode
+[      1.970]finally, output_type=0x4, output_mode=0x4, screen_id=0x0, disp_para=0x0
+fail to find part named env
+Using default environment
+
 In:    serial
 Out:   serial
 Err:   serial
-Net:   No ethernet found.
-starting USB...
-USB0:   USB EHCI 1.00
-USB1:   USB EHCI 1.00
-scanning bus 0 for devices... 1 USB Device(s) found
-scanning bus 1 for devices... 1 USB Device(s) found
-Hit any key to stop autoboot:  2 
-switch to partitions #0, OK
-mmc0 is current device
-Scanning mmc 0:1...
-Found U-Boot script /boot.scr
-228 bytes read in 65 ms (2.9 KiB/s)
-## Executing script at 43100000
-36856 bytes read in 167 ms (214.8 KiB/s)
-8423416 bytes read in 8948 ms (918.9 KiB/s)
-## Booting kernel from Legacy Image at 48000000 ...
-   Image Name:   Linux-3.4.39-1mni
-   Image Type:   ARM Linux Kernel Image (uncompressed)
-   Data Size:    8423352 Bytes = 8 MiB
-   Load Address: 40008000
-   Entry Point:  40008000
-   Verifying Checksum ... OK
-   Loading Kernel Image ... OK
-Starting kernel ...
+--------fastboot partitions--------
+mbr not exist
+base bootcmd=run setargs_mmc boot_normal
+bootcmd set setargs_mmc
+key 0
+cant find rcvy value
+cant find fstbt value
+no misc partition is found
+to be run cmd=run setargs_mmc boot_normal
+[      2.007][mmc]: MMC Device 2 not found
+[      2.011][mmc]: Can not find mmc dev
+[      2.014][mmc]: read first backup failed in fun sdmmc_secure_storage_read line 1849
+sunxi_secstorage_read fail
+get secure storage map err
+the private part isn't exist
+WORK_MODE_BOOT
+adver not need show
+sunxi_bmp_logo_display
+[disk_read_fs] no the partition
+error: open bootlogo.bmp, maybe it is not exist
+sunxi bmp info error : unable to open logo file bootlogo.bmp
+[      2.047]Hit any key to stop autoboot:  5 4 3 2 1 0 
+[      7.489][mmc]: Should not w/r secure area in fun mmc_bread_secure,line,1696 in start 2489,end 18936
+Error reading cluster
+
+** Unable to read "uimage" from mmc 0:1 **
+Wrong Image Format for bootm command
+ERROR: can't get kernel image!
+sunxi#
 ```
--- mybuild.sh
+++ mybuild.sh
@@ -2,113 +2,130 @@
 
 # ./OrangePI-Kernel/build/config.good/sun8iw7p1smp_lobo_defconfig.opiplus
 
-TMPDIR=`mktemp -d /tmp/XXXOPI_$$`
+TMPF=`mktemp -d /tmp/OPIXXX_$$`
 CURD=`pwd`
-OUTFOLDER=build
+OUTDIR=build
 SRCFOLDER=OrangePiPlus-Kernel
 LOBOSRCDIR=${SRCFOLDER}/OrangePI-Kernel
-SD_CARD_FILE=./nsdcard.img
-ROOTPWD="toor" # Must be changed @ first login
+SD_CARD_FILE=./sdcard.img
+ROOTPWD="toor" # Must be changed @first login
 
 end()
 {
-	echo "Deleting $TMPDIR"
-        sudo rm -rf $TMPDIR
+        sudo rm -rf $TMPF
         exit 0
 }
 
 trap end 9 2
 
+# $1 = exit code
+# $2 the message
+myexit () {
+	echo $2
+	sudo rm -rf $TMPF
+	rm -f $SD_CARD_FILE
+	exit $1
+}
+
 get_sources() {
 	if [ -d $SRCFOLDER ]; then
-		echo "Git source folder ${SRCFOLDER} exists, updating ..."
+		echo "Source folder ${SRCFOLDER} exists, updating ..."
 		(cd $SRCFOLDER ; git pull)
 	else
 		git clone https://github.com/j75/OrangePiPlus-Kernel.git
 	fi
-
-	if [ -d u-boot ]; then
-		echo "Git u-boot folder exists, updating ..."
-		(cd u-boot ; git pull)
-	else
-		git clone git://git.denx.de/u-boot.git
-	fi
 }
 
-build_fex_script() {
+build_script() {
 	echo "Building script.bin.${1}_${2}_${3}"
 
-	cp ${LOBOSRCDIR}/build/orange_pi_plus.fex ${OUTFOLDER}/sys_config.fex
+	#cp ${LOBOSRCDIR}/build/orange_pi_plus.fex ${OUTDIR}/sys_config.fex
+	grep -v "^;" ${LOBOSRCDIR}/build/orange_pi_plus.fex > ${OUTDIR}/sys_config.fex
 	# 720p50
-	cat ${OUTFOLDER}/sys_config.fex | sed s/"screen0_output_mode      = 10"/"screen0_output_mode      = 4"/g > ${OUTFOLDER}/_sys_config.fex
-	mv ${OUTFOLDER}/_sys_config.fex ${OUTFOLDER}/sys_config.fex
-	cat ${OUTFOLDER}/sys_config.fex | sed s/"screen1_output_mode      = 10"/"screen1_output_mode      = 4"/g > ${OUTFOLDER}/_sys_config.fex
-	mv ${OUTFOLDER}/_sys_config.fex ${OUTFOLDER}/sys_config.fex
+	cat ${OUTDIR}/sys_config.fex | sed s/"screen0_output_mode      = 10"/"screen0_output_mode      = 4"/g > ${OUTDIR}/_sys_config.fex
+	mv ${OUTDIR}/_sys_config.fex ${OUTDIR}/sys_config.fex
+	cat ${OUTDIR}/sys_config.fex | sed s/"screen1_output_mode      = 10"/"screen1_output_mode      = 4"/g > ${OUTDIR}/_sys_config.fex
 	# max. frequency: 1.2 GHz
+	perl -pi -e "s|^extremity_freq.*|extremity_freq     = 1200000000|g" ${OUTDIR}/_sys_config.fex
+	
+	mv ${OUTDIR}/_sys_config.fex ${OUTDIR}/sys_config.fex
+}
+
+# http://linux-sunxi.org/H3_Manual_build_howto
+build_fex_script() {
+	if [ ! -f ${OUTDIR}/sys_config.fex ]; then
+		myexit 11 "No ${OUTDIR}/sys_config.fex file!"
+	fi
+
+	echo "Fex file compiler requires fex file in CRLF format"
+	unix2dos ${OUTDIR}/sys_config.fex
+	echo "${LOBOSRCDIR}/pctools/linux/mod_update/script"
+	${LOBOSRCDIR}/pctools/linux/mod_update/script ${OUTDIR}/sys_config.fex
+	echo "Compile fex file"
+	fex2bin ${OUTDIR}/sys_config.fex ${OUTDIR}/sys_config.bin
+	echo "Patch sdcard boot0"
+	cp ${LOBOSRCDIR}/chips/sun8iw7p1/bin/boot0_sdcard_sun8iw7p1.bin ${OUTDIR}/boot0_sdcard.fex
+	echo "${LOBOSRCDIR}/pctools/linux/mod_update/update_boot0"
+	${LOBOSRCDIR}/pctools/linux/mod_update/update_boot0 ${OUTDIR}/boot0_sdcard.fex ${OUTDIR}/sys_config.bin SDMMC_CARD
 }
 
 build_bin () {
-	if [ -d ${OUTFOLDER} ]; then
-		echo "Output folder ${OUTFOLDER} exists, ..."
+	if [ -d ${OUTDIR} ]; then
+		echo "Output folder ${OUTDIR} exists, ..."
 	else
-		mkdir ${OUTFOLDER}
+		mkdir ${OUTDIR}
 
-		build_fex_script "OPI-PLUS" "720p50" "hdmi"
+		build_script "OPI-PLUS" "720p50" "hdmi"
 	fi
 }
 
-set_cross_compiler() {
-	# arm-linux-gnueabihf-gcc
-	CROSS_COMP=arm-linux-gnueabihf
-	which ${CROSS_COMP}-gcc >/dev/null
-	if [ $? -eq 0 ]; then
-		echo -n "Using cross-compiler "
-		which ${CROSS_COMP}-gcc
+
+build_uboot_legacy () {
+	if [ -f ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin/u-boot-sun8iw7p1.bin ]; then
+		echo "u-boot-sun8iw7p1.bin was previously created"
 	else
+		echo "Building U-Boot legacy"
+		if [ -d ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin ]; then
+			rm -f ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin/u-boot*
+			echo "Folder ${SRCFOLDER}/tools/pack/chips/sun8iw7p1/bin has been cleaned"
+		else
+			mkdir -p ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin
+			echo "Folder ${SRCFOLDER}/tools/pack/chips/sun8iw7p1/bin has been created"
+		fi
+		LCC=`pwd`/${LOBOSRCDIR}/brandy/gcc-linaro/bin
+		export PATH="${LCC}":"$PATH"
 		CROSS_COMP=arm-linux-gnueabi
-		which ${CROSS_COMP}-gcc >/dev/null
-		if [ $? -gt 0 ]; then
-			LCC=`pwd`/OrangePiPlus-Kernel/OrangePI-Kernel/brandy/gcc-linaro/bin
-			export PATH="${LCC}":"$PATH"
-		else
-			echo -n "Using cross-compiler "
-			which ${CROSS_COMP}-gcc
-		fi
+				
+		HERE=`pwd`
+		cd ${LOBOSRCDIR}/brandy/u-boot-2011.09
+			make ARCH=arm mrproper
+			echo "make ARCH=arm CROSS_COMPILE=${CROSS_COMP}- sun8iw7p1_config"
+			make ARCH=arm CROSS_COMPILE=${CROSS_COMP}- sun8iw7p1_config
+			if [ $? -gt 0 ]; then
+				myexit 8 "error cross-compiling config file"
+			fi
+			make ARCH=arm CROSS_COMPILE=${CROSS_COMP}-
+
+			if [ $? -gt 0 ]; then
+				myexit 7 "error cross-compiling"
+			fi
+		cd $HERE
 	fi
 }
 
-# https://linux-sunxi.org/Mainline_U-boot
-build_uboot_new () {
-	if [ -f u-boot/u-boot-sunxi-with-spl.bin ]; then
-		echo "Good - u-boot-sunxi-with-spl.bin exists"
-	else
-		echo "Building U-Boot mainline"
-		if [ -f u-boot/u-boot-sunxi-with-spl.bin ]; then
-			echo "u-boot-sun8iw7p1.bin was previously created"
-		else
-			set_cross_compiler
-			# TODO -DCONFIG_SYS_BOOTM_LEN=0xF00000
-			echo "Preventing 'Image too large: increase CONFIG_SYS_BOOTM_LEN'"
-			perl -pi -e 's|^#define CONFIG_SYS_BOOTM_LEN.*|#define CONFIG_SYS_BOOTM_LEN    0xFFF000|' ./u-boot/common/bootm.c
-			cd u-boot
-			make ARCH=arm mrproper
-			echo "make CROSS_COMPILE=${CROSS_COMP}- orangepi_plus_defconfig"
-			make CROSS_COMPILE=${CROSS_COMP}- orangepi_plus_defconfig
-			echo "make CROSS_COMPILE=${CROSS_COMP}-"
-			#make CROSS_COMPILE=${CROSS_COMP}- V=1
-			make CROSS_COMPILE=${CROSS_COMP}-
-			RES_COMP_UBOOT=$?
-			git checkout common/bootm.c
-			if [ $RES_COMP_UBOOT -gt 0 ]; then
-				echo "Error building u-boot"
-				exit 9
-			else
-				echo "OK u-boot creation"
-				ls -alF u-boot-sunxi-with-spl.bin u-boot
-			fi
-			cd ..
-		fi
+# http://linux-sunxi.org/H3_Manual_build_howto
+patch_uboot() {
+	if [ ! -f ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin/u-boot-sun8iw7p1.bin ]; then
+		myexit 9 "No ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin/u-boot-sun8iw7p1.bin file!"
 	fi
+	if [ ! -f ${OUTDIR}/sys_config.bin ]; then
+		myexit 10 "No ${OUTDIR}/sys_config.bin file!"
+	fi
+
+	echo "Copy u-boot"
+	cp ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin/u-boot-sun8iw7p1.bin ${OUTDIR}/u-boot.fex
+	echo "Patch u-boot"
+	${LOBOSRCDIR}/pctools/linux/mod_update/update_uboot ${OUTDIR}/u-boot.fex ${OUTDIR}/sys_config.bin
 }
 
 build_kernel () {
@@ -120,19 +137,26 @@ build_kernel () {
 	fi
 }
 
+# http://linux-sunxi.org/H3_Manual_build_howto Installing onto Storage -> SD card
 # Secondary program loader + u-Boot
-write_initial_SPL_anduBoot () {
-	dd if=./u-boot/u-boot-sunxi-with-spl.bin of=$1 bs=1024 seek=8
-	echo "Secondary program loader and u-Boot (u-boot-sunxi-with-spl.bin)"
-    echo "    have been written on $1 after first 8k"	
-}
-
-# SPL + u-Boot
-write_separate () {
-	dd if=./u-boot/spl/sunxi-spl.bin of=$1 bs=1024 seek=8 conv=notrunc
-	echo "SPL loader u-boot/spl/sunxi-spl.bin has been written on $1"
-	dd if=./u-boot/u-boot.bin of=$1 bs=1024 seek=32 conv=notrunc
-	echo "Das U-Boot u-boot/u-boot.bin has been written on $1"
+write_initial_SPL_uBoot () {
+	SPLFILE="${OUTDIR}/u-boot.fex"
+	if [ ! -f $SPLFILE ]; then
+		myexit 1 "Cannot find SPL file $SPLFILE - exiting..."
+	fi
+	# write boot0
+	dd if=${OUTDIR}/boot0_sdcard.fex of=$1 bs=1k seek=8
+	if [ $? -gt 0 ]; then
+		myexit 6 "Dumping ${OUTDIR}/boot0_sdcard.fex file to $1 error - exiting..."
+	fi
+	echo "boot0 (${OUTDIR}/boot0_sdcard.fex) has been written on $1 after first 8k"
+	# write u-boot
+	dd if=$SPLFILE of=$1 bs=1k seek=16400
+	if [ $? -gt 0 ]; then
+		myexit 7 "Dumping SPL file $SPLFILE to $1 error - exiting..."
+	fi
+	echo "Secondary program loader (${SPLFILE}) has been written on $1 after first 16M"
+	
 }
 
 # Partition the card with a 100MB boot partition starting at 1MB (type = c = fwin95)
@@ -145,6 +169,8 @@ p
 1
 2048
 +100M
+t
+c
 n
 p
 
@@ -159,24 +185,10 @@ format() {
 	echo "Loop device is $LOOPDEV"
 	sudo kpartx -av $LOOPDEV
 	BASELDEV=`echo $LOOPDEV | cut -d '/' -f3`
-	mkfs.ext2 -L BOOT  /dev/mapper/${BASELDEV}p1
-	if [ $? -gt 0 ]; then
-		echo "Error formating /dev/mapper/${BASELDEV}p1"
-		sudo kpartx -dv $LOOPDEV
-		losetup -d $LOOPDEV
-		exit 7
-	fi
-	FORMATP2=""
+	mkfs.vfat -n BOOT  /dev/mapper/${BASELDEV}p1
 	mkfs.ext4 -L Linux /dev/mapper/${BASELDEV}p2
-	if [ $? -gt 0 ]; then
-		echo "Error formating /dev/mapper/${BASELDEV}p2"
-		FORMATP2="ko"
-	fi
 	sudo kpartx -dv $LOOPDEV
 	losetup -d $LOOPDEV
-	if [ -n $ FORMATP2 ]; then
-		exit 8
-	fi
 }
 
 copy_file() {
@@ -188,46 +200,29 @@ copy_file() {
 	fi
 }
 
-convert_fex2bin ()
-{
-	grep -v "^;" ${OUTFOLDER}/sys_config.fex | grep -v "^#" > script.fex
-	perl -pi -e 's|^extremity_freq.*|extremity_freq = 1200000000|g' script.fex
-	perl -pi -e 's|^max_freq.*|max_freq = 1100000000|g' script.fex
-	echo "Converting script.fex -> script.bin"
-	fex2bin script.fex script.bin
-}
-
 copy_boot_partition() {
 	LOOPDEV=`losetup -f --show $SD_CARD_FILE`
 	echo "Loop device is $LOOPDEV"
 	sudo kpartx -av $LOOPDEV
 	BASELDEV=`echo $LOOPDEV | cut -d '/' -f3`
-	sudo mount /dev/mapper/${BASELDEV}p1 $TMPDIR
-	if [ $? -gt 0 ]; then
-		echo "Error mounting /dev/mapper/${BASELDEV}p1 -> $TMPDIR"
-		sudo kpartx -dv $LOOPDEV
-		losetup -d $LOOPDEV
-		exit 1
-	fi
-	copy_file boot.scr $TMPDIR
+	sudo mount /dev/mapper/${BASELDEV}p1 $TMPF
+	copy_file boot.scr $TMPF
 	rm boot.scr
-	#copy_file fex $TMPDIR
-	echo "Transferring uImage kernel to SD card boot partition"
-	ls -alF ${SRCFOLDER}/output/uImage*
-	sudo cp -a ${SRCFOLDER}/output/uImage* $TMPDIR
-	#
-	convert_fex2bin
-	copy_file script.fex $TMPDIR
-	copy_file script.bin $TMPDIR
+	#copy_file fex $TMPF
+	copy_file ${SRCFOLDER}/output/uImage $TMPF
+	grep -v "^;" ${OUTDIR}/sys_config.fex | grep -v "^#" > script.fex
+	perl -pi -e 's|^max_freq.*|max_freq = 1100000000|g' script.fex
+	fex2bin script.fex script.bin
+	copy_file script.fex $TMPF
+	copy_file script.bin $TMPF
 	rm -f script.*
 	sudo sync
 	#
 	echo "Boot partition OK:"
-	ls -alF $TMPDIR
-	sudo umount $TMPDIR
+	ls -alF $TMPF
+	sudo umount $TMPF
 	if [ $? -gt 0 ]; then
-		echo "Error unmounting $TMPDIR"
-		exit 2
+		myexit 2 echo "Error unmounting $TMPF"
 	fi
 	sudo kpartx -dv $LOOPDEV
 	losetup -d $LOOPDEV
@@ -237,8 +232,8 @@ make_uboot_commands () {
 	echo "Creating boot.cmd file"
 	cat << EOF > boot.cmd
 setenv bootargs console=ttyS0 root=/dev/mmcblk0p1 rootwait panic=10 ${extra}
-ext2load mmc 0 0x43000000 script.bin
-ext2load mmc 0 0x48000000 uImage
+ext2load mmc 0 0x43000000 boot/script.bin
+ext2load mmc 0 0x48000000 boot/uImage
 bootm 0x48000000
 EOF
 	echo "Converting boot.cmd -> boot.scr"
@@ -250,69 +245,41 @@ copy_root_partition() {
 	echo "Loop device is $LOOPDEV"
 	sudo kpartx -av $LOOPDEV
 	BASELDEV=`echo $LOOPDEV | cut -d '/' -f3`
-	sudo mount /dev/mapper/${BASELDEV}p2 $TMPDIR
-	if [ $? -gt 0 ]; then
-		echo "Error mounting /dev/mapper/${BASELDEV}p2 -> $TMPDIR"
-		sudo kpartx -dv $LOOPDEV
-		losetup -d $LOOPDEV
-		exit 3
-	fi
+	sudo mount /dev/mapper/${BASELDEV}p2 $TMPF
 	#
-	echo "Transferring ${SRCFOLDER}/output/lib to SD card file"
-	sudo cp -r ${SRCFOLDER}/output/lib $TMPDIR
-	# rm build/source
-	sudo rm -f ${TMPDIR}/lib/modules/*/{build,source}
-	#
-	sudo mkdir ${TMPDIR}/boot
-	echo "Transferring kernel to SD card file root partition"
-	ls -alF ${SRCFOLDER}/output/*-*
-	sudo cp ${SRCFOLDER}/output/*-* ${TMPDIR}/boot
-	#
-	sudo cp u-boot/System.map ${TMPDIR}/boot/u-boot-System.map
+	sudo cp -r ${SRCFOLDER}/output/lib $TMPF
+	sudo mkdir ${TMPF}/boot
+	sudo cp ${SRCFOLDER}/output/*-* ${TMPF}/boot
 	#
 	distro="jessie"
 	echo "Loading Debian $distro distribution"
-	sudo debootstrap --include=openssh-server --arch=armhf --foreign $distro $TMPDIR
+	sudo debootstrap --include=openssh-server,bash --arch=armhf --foreign $distro $TMPF
 	if [ $? -gt 0 ]; then
 		echo "Error creating Debian distribution"
 	else
-		echo "nameserver 127.0.1.1" | sudo tee ${TMPDIR}/etc/resolv.conf
-		sudo sh -c "echo 'orangepiplus' >  ${TMPDIR}/etc/hostname"
-		#
+		echo "nameserver 127.0.1.1" | sudo tee ${TMPF}/etc/resolv.conf
+		sudo sh -c "echo 'orangepiplus' >  ${TMPF}/etc/hostname"
 		cat <<EOT > /tmp/sources.list
 deb http://http.debian.net/debian $distro main contrib non-free
-#deb-src http://http.debian.net/debian $distro main contrib non-free
-
+deb-src http://http.debian.net/debian $distro main contrib non-free
 deb http://http.debian.net/debian $distro-updates main contrib non-free
-#deb-src http://http.debian.net/debian $distro-updates main contrib non-free
-
+deb-src http://http.debian.net/debian $distro-updates main contrib non-free
 deb http://security.debian.org/debian-security $distro/updates main contrib non-free
-#deb-src http://security.debian.org/debian-security $distro/updates main contrib non-free
+deb-src http://security.debian.org/debian-security $distro/updates main contrib non-free
 EOT
-		sudo cp -f /tmp/sources.list ${TMPDIR}/etc/apt/
-		#
-		sudo rm -rf ${TMPDIR}/debootstrap
+		sudo cp -f /tmp/sources.list ${TMPF}/etc/apt/
 		echo "Setting password $ROOTPWD -> should be in an ARM chrooted environment"
-		#sudo chroot ${TMPDIR} /bin/bash -c "(echo $ROOTPWD;echo $ROOTPWD;) | passwd root"
-		sudo sh -c "echo 'root:x:0:0:root:/root:/bin/bash' >  ${TMPDIR}/etc/passwd"
-		sudo sh -c "echo 'daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin' >>  ${TMPDIR}/etc/passwd"
-		sudo sh -c "echo 'bin:x:2:2:bin:/bin:/usr/sbin/nologin' >>  ${TMPDIR}/etc/passwd"
-		sudo sh -c "echo 'sys:x:3:3:sys:/dev:/usr/sbin/nologin' >>  ${TMPDIR}/etc/passwd"
-		sudo sh -c "echo 'nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin' >>  ${TMPDIR}/etc/passwd"
-		sudo sh -c "echo 'root:\$6\$j3USIojv\$Vc1VrKa3j0MKXaLrEZyc2w3PnbRpBIPmt0ULvnIqquMPkWKW1fim4PBn.m0iC5BaZq609o27x3TIp8D0GKKBj/:16882:0:99999:7:::' >>  ${TMPDIR}/etc/shadow"
-		sudo chmod 640 ${TMPDIR}/etc/shadow
-		#
+		#sudo chroot ${TMPF} /bin/bash -c "(echo $ROOTPWD;echo $ROOTPWD;) | passwd root"
 		echo "Debian loaded"
 	fi
 	#
 	sudo sync
 	#
 	echo "Linux partition OK:"
-	ls -alF $TMPDIR
-	sudo umount $TMPDIR
+	ls -alF $TMPF
+	sudo umount $TMPF
 	if [ $? -gt 0 ]; then
-		echo "Error unmounting $TMPDIR"
-		exit 4
+		myexit 3 "Error unmounting $TMPF"
 	fi
 	sudo kpartx -dv $LOOPDEV
 	losetup -d $LOOPDEV
@@ -323,8 +290,8 @@ make_sd_card () {
 		echo "SD card file $SD_CARD_FILE exists, won't overwrite..."
 	else
 		dd if=/dev/zero of=${SD_CARD_FILE} bs=1M count=1
-		write_initial_SPL_anduBoot ${SD_CARD_FILE}
-		dd if=/dev/zero bs=1M count=1600 of=${SD_CARD_FILE} oflag=append conv=notrunc 
+		write_initial_SPL_uBoot ${SD_CARD_FILE}
+		dd if=/dev/zero bs=1M count=1600 of=${SD_CARD_FILE} oflag=append conv=notrunc
 		#>> ${SD_CARD_FILE}
 		echo "1.6 G added at the end of ${SD_CARD_FILE}"
 		partition ${SD_CARD_FILE}
@@ -340,40 +307,35 @@ make_sd_card () {
 check_exec () {
 	which $1 > /dev/null
 	if [ $? -gt 0 ]; then
-		echo "$1 not found ... exiting"
-		exit 5
-	else
-		echo "  $1 exists ... good!"
+		myexit 4 "$1 not found ... exiting"
 	fi
 }
 
 check_group() {
 	id | grep $1 >/dev/null
 	if [ $? -gt 0 ]; then
-		echo "User does not belong to group $1 ... exiting"
-		exit 6
-	else
-		echo "  user belongs to group $1 ... good!"
+		myexit 5 "User does not belong to group $1 ... exiting"
 	fi
 }
 
 check_requirements() {
 	echo "Checking requirements for building..."
+	if [ "`arch`" -ne "x86_64" ]; then
+		myexit 7 "Bad architecture..."
+	fi
 	# kpartx mount.vfat losetup, dd,...
 	check_exec dd
-	check_exec chmod
 	check_exec losetup
 	check_exec kpartx
-	check_exec mkfs.ext2
+	check_exec mkfs.vfat
 	check_exec mkfs.ext4
 	check_exec debootstrap
 	#check_exec cdebootstrap
 	check_exec chroot
-	check_exec perl
-	check_exec fex2bin
+	check_exec unix2dos
 	check_group disk
 	check_group sudo
-	echo "All requirements are fullfiled!"
+	echo "Good - all requirements are fullfiled!"
 }
 
 build_all () {
@@ -381,11 +343,17 @@ build_all () {
 	get_sources
 	build_kernel
 	build_bin
-	if [ ! -f u-boot/u-boot-sunxi-with-spl.bin ]; then
-		build_uboot_new
+	if [ ! -f ${LOBOSRCDIR}/tools/pack/chips/sun8iw7p1/bin/u-boot-sun8iw7p1.bin ]; then
+		build_uboot_legacy
 	fi
+	build_fex_script
+	#
+	patch_uboot
 	#
 	make_sd_card
+	#
+	echo "Now you may do, as root"
+	echo "dd if=sdcard.img of=/dev/sdd bs=1M oflag=direct"
 }
 
 build_all
